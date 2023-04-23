ca1673097674d2c881500e2d79169b5981b08b60
wxzzxrdresurrection <magicwizz12@gmail.com>
2023-04-22 21:51:13 -0600

    config

--- config/mongo.ts
+++ config/mongo.ts
@@ -3,10 +3,34 @@ import mongoose from 'mongoose'
 
 
 mongoose.connect(
-  Env.get('MONGO_URL', 'mongodb://localhost:27017/adonis')).then(() => {
+  Env.get('MONGO_URL1', 'mongodb://localhost:27017/adonis')).then(() => {
     console.log('MongoDB Connected')
-  }).catch(err => console.log(err))
+  }).catch(
+    () =>{
+      mongoose.connect(
+        Env.get('MONGO_URL2', 'mongodb://localhost:27018/adonis')).then(() => {
+          console.log('MongoDB Connected')
+        }).catch(
+          () =>{
+            mongoose.connect(
+              Env.get('MONGO_URL3', 'mongodb://localhost:27019/adonis')).then(() => {
+                console.log('MongoDB Connected')
+              }).catch(
 
+                () =>{
+                  mongoose.connect(
+                    Env.get('MONGO_URL1', 'mongodb://localhost:27017/adonis')).then(() => {
+                      console.log('MongoDB Connected')
+                    }).catch(err => console.log(err))
+                }
+              )
+              
+          }
+
+        )
+    }
+
+  )
 
 export default mongoose
 
